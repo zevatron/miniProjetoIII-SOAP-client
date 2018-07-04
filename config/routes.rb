1b707f06af24db3f5fea6b41917f85bac1802b6a
José Ricardo <zevatron@zevatron.com.br>
2018-07-04 09:01:21 -0300

    Ajuste de rota root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :filmes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   delete 'filmes', to: 'filmes#destroy'
+  root 'filmes#index'
 end
